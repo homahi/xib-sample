0ec10d239e5e0a387fcc5b0b7f32b98ad285f95f
homahi <homahi0128@gmail.com>
2018-02-12 16:43:16 +0900

    init

--- XibPractice/ViewController.swift
+++ XibPractice/ViewController.swift
@@ -9,16 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.loadTemplate()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func loadTemplate() {
+        let view: UIView = UINib(nibName: "SampleView", bundle: nil).instantiate(withOwner: self, options: nil)[0] as! UIView
+        self.view.addSubview(view)
+    }
+    
 
 
 }
